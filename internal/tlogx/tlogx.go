a775d9d3efc8f4779b672df1e9847a1aef393505
Filippo Valsorda <hi@filippo.io>
2024-10-08 16:39:40 +0200

    internal/tlogx: drop some unused code

--- internal/tlogx/tlogx.go
+++ internal/tlogx/tlogx.go
@@ -7,38 +7,6 @@ package tlogx
 
 import "golang.org/x/mod/sumdb/tlog"
 
-// TileParent returns t's k'th tile parent in the tiles for a tree of size n.
-// If there is no such parent, ok is false.
-func TileParent(t tlog.Tile, k int, n int64) (parent tlog.Tile, ok bool) {
-	t.L += k
-	t.N >>= k * t.H
-	t.W = 1 << t.H
-	if max := n >> (t.L * t.H); t.N<<t.H+int64(t.W) >= max {
-		if t.N<<t.H >= max {
-			return parent, false
-		}
-		t.W = int(max - t.N<<t.H)
-	}
-	return t, true
-}
-
-// PartialTiles returns the partial tiles for a tree of size n.
-func PartialTiles(h int, n int64) []tlog.Tile {
-	var partial []tlog.Tile
-	t := tlog.TileForIndex(h, tlog.StoredHashIndex(0, n-1))
-	for {
-		if t.W < 1<<t.H {
-			partial = append(partial, t)
-		}
-		var ok bool
-		t, ok = TileParent(t, 1, n)
-		if !ok {
-			break
-		}
-	}
-	return partial
-}
-
 // RightEdge returns the stored hash indexes of the right edge of a tree of
 // size n. These are the same hashes that are combined into a [tlog.TreeHash]
 // and allow producing record and tree proofs for any size bigger than n. See
